a978f20dc9e15212c18bd6f53eced6435f86bcad
whiteheadbria <whitehead.bria@protonmail.com>
2022-09-09 10:52:58 -0500

    Finished the Challenge 1.1 with creating your initials. and also adding the class compile file to run the program

--- Module 1 - Introduction to Java/1. Getting Started/Challenge 1/YourInitials.java
+++ Module 1 - Introduction to Java/1. Getting Started/Challenge 1/YourInitials.java
@@ -1,5 +1,34 @@
 public class YourInitials {
     public static void main(String[] args) {
         //print initials here. 
+        // initials are bnw
+
+        System.out.println("BBBB ");
+        System.out.println("B   B");
+        System.out.println("B   B");
+        System.out.println("BBBB ");
+        System.out.println("B   B");
+        System.out.println("B   B");
+        System.out.println("BBBB ");
+
+        System.out.println(" ");
+
+        System.out.println("N   N");
+        System.out.println("NN  N");
+        System.out.println("N N N");
+        System.out.println("N  NN");
+        System.out.println("N   N");
+        System.out.println("N   N");
+        System.out.println("N   N");
+
+        System.out.println(" ");
+
+        System.out.println("W   W");
+        System.out.println("W   W");
+        System.out.println("W   W");
+        System.out.println("W W W");
+        System.out.println("WW WW");
+        System.out.println("WW WW");
+        System.out.println("W   W");
     }
 }
