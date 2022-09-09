bc24f4fe51f3a142a9c41947125dfc32d1aaadc3
whiteheadbria <whitehead.bria@protonmail.com>
2022-09-09 11:52:27 -0500

    updated the workbook 2.1 based on the activity direction. updating points scored

--- Module 1 - Introduction to Java/2. Variables/Workbook 2.1/Points.java
+++ Module 1 - Introduction to Java/2. Variables/Workbook 2.1/Points.java
@@ -3,15 +3,24 @@ public class Points {
         int points = 0;
 
         System.out.println("Harry was caught wandering the halls. -50 points for Gryffindor");
-        //update points here
+        points -= 50;
+        System.out.println(points);
+
         System.out.println("Harry was being cheeky in class. -3 points for Gryffindor");
-        //update points here
+        points -= 3;
+        System.out.println(points);
+
         System.out.println("Hermione got full marks on Lockhart's quiz. 30 points for Gryffindor");
-        //update points here
+        points += 30;
+        System.out.println(points);
+
         System.out.println("Ron won the underground chess game. 100 points for Gryffindor");
-        //update points here
+        points += 100;
+        System.out.println(points);
+
         System.out.println("Harry defeated Quirrell. 60 points for Gryffindor");
-        //update points here
+        points += 100;
+        System.out.println(points);
 
         //Print the variable
     }
