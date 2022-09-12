97c089a2c6774570a14850fe5887615e42651611
Bria Whitehead <whitehead.bria@protonmail.com>
2022-09-12 12:43:06 -0500

    Completed the 2.4 Workbook assignment on Typecasting

--- Module 1 - Introduction to Java/2. Variables/Workbook 2.4/Meeting.java
+++ Module 1 - Introduction to Java/2. Variables/Workbook 2.4/Meeting.java
@@ -1,14 +1,24 @@
 public class Meeting {
     public static void main(String[] args) {
         double sales = 24309.65; 
-        double profit = 18562.18; 
-        double refunds = 688.78; 
-        double shipping = 1233.57; 
+        int roundedSales = (int)sales;
 
-        System.out.println("This month, we made $<sales> in sales");
-        System.out.println("Factoring in costs, we made $<profit> in profit");
-        System.out.println("The refunds are at a low $<refunds>. This is a good sign!");
-        System.out.println("Shipping costs were high. We paid $<shipping> in shipping");
+        double profit = 18562.18; 
+        int roundedProfit = (int)profit;
+
+        double refunds = 688.78; 
+        int roundedRefunds = (int)refunds;
+
+        double shipping = 1233.57; 
+        int roundedShipping = (int)shipping;
+
+        System.out.println("This month, we made $" + roundedSales + " in sales");
+
+        System.out.println("Factoring in costs, we made $" + roundedProfit + " in profits");
+
+        System.out.println("The refunds are at a low $" + roundedRefunds + ". This is a good sign!");
+
+        System.out.println("Shipping costs were high. We paid $" + roundedShipping + " in shipping");
 
 
     }
