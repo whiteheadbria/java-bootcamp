8857de7cf23d415726f0b9b550d32b7b81d91988
Bria Whitehead <whitehead.bria@protonmail.com>
2022-09-12 12:35:27 -0500

    type casting. name should me TypeConversion, not TypeConversation

---
+++ Module 1 - Introduction to Java/2. Variables/Section Two/TypeConversation.java
@@ -0,0 +1,9 @@
+public class TypeConversation {
+    public static void main(String[] args) {
+
+        double salary = 4445.231122;
+        int roundedSalary = (int)salary;
+        System.out.println("Anglea's monthly salary is $" + roundedSalary);
+
+    }
+}
