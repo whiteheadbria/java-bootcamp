90ff3689f5a01ded0ba30e51e82bf917e5c7085e
Bria Whitehead <whitehead.bria@protonmail.com>
2022-09-12 14:46:51 -0500

    Completed the final assignment in section two. it's 2.5 workbook signup workbook

--- Module 1 - Introduction to Java/2. Variables/Workbook 2.5/SignUp.java
+++ Module 1 - Introduction to Java/2. Variables/Workbook 2.5/SignUp.java
@@ -11,20 +11,49 @@ public class SignUp {
         //Task 1 - Using Scanner, ask the user questions
 
         //Ask for their first name.
+        System.out.println("\nWhat is your first name?");
+        String firstName = scan.nextLine();
+
         //Ask for their last name.
+        System.out.println("What is your last name?");
+        String lastName = scan.nextLine();
+
         //Ask: how old are you?
+        System.out.println("How old are you?");
+        int age = scan.nextInt();
+
         /* Ask them to make a username. SIDE NOTE:
            – nextLine() gets "skipped" if you put it ahead of nextInt() , nextDouble(), nextLong().
            – You will understand why this happens when we cover Delimiters in the next section (Booleans and Conditionals).
            – For now, just know that the solution is to add an extra nextLine().
         */
-        //Ask what city they live in.
-        //Ask what country that's from.
 
+        scan.nextLine();
+
+        // Ask to create a username
+        System.out.println("Please create a username: ");
+        String userName = scan.nextLine();
+
+        //Ask what city they live in.
+        System.out.println("What city are you from?");
+        String city = scan.nextLine();
+
+        //Ask what country that's from.
+        System.out.println("What country are you from?");
+        String country = scan.nextLine();
 
         //Task 2 - Print their information. 
+        System.out.println("\nThank you for joining JavaGram!");
 
-        System.out.println("Thank you for joining JavaGram!");
+        scan.close();
+
+        System.out.println("\nYour information:");
+        System.out.println("First name: " + firstName);
+        System.out.println("Last name: " + lastName);
+        System.out.println("Age: " + age);
+        System.out.println("Username: " + userName);
+        System.out.println("City: " + city);
+        System.out.println("Country: " + country);
 
         //Print their information like so:
 
