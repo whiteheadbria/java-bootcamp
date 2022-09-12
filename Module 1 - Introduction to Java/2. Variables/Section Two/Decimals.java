d8496092a6f1d3ad69d3f9ddeb621ef4ebda736d
Bria Whitehead <whitehead.bria@protonmail.com>
2022-09-12 10:20:41 -0500

    2.3 assignments

---
+++ Module 1 - Introduction to Java/2. Variables/Section Two/Decimals.java
@@ -0,0 +1,14 @@
+public class Decimals {
+
+    public static void main(String[] args) {
+        double percentage = 55.7;
+        System.out.println("In 2020, " + percentage + "% of the world's population is urban");
+
+        double divisor = 2; //double when we want exact with decimals
+        double dividend = 25;
+        
+        System.out.println(dividend + " divided by " + divisor + " = " + dividend/divisor);
+        
+    }
+    
+}
---
+++ Module 1 - Introduction to Java/2. Variables/Section Two/WholeNumbers.java
@@ -0,0 +1,15 @@
+public class WholeNumbers {
+
+    public static void main (String[] args) {
+        int numberOfFans = 5000; 
+        System.out.println("There are " + numberOfFans + " at the Formula One meet and greet");
+
+        long globalPopulation = 8000000000L;
+        System.out.println("There are " + globalPopulation + " people on earth");
+
+        long dailyGoogleSearches = 5000000000L;
+        System.out.println("Daily, there are " + dailyGoogleSearches + " google searches a day");
+
+
+    }
+}
