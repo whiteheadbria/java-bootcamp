e59cb671d30cee4d86e8c436ad0162cf81796221
Bria Whitehead <whitehead.bria@protonmail.com>
2022-09-13 12:26:38 -0500

    Completed assignment for If Else statements

---
+++ Module 1 - Introduction to Java/3. Booleans and Conditionals/Section 3/IfElse.java
@@ -0,0 +1,16 @@
+public class IfElse {
+    public static void main (String[] args) {
+
+        int grade = 65;
+        
+        if (grade > 50) {
+            System.out.println("Great job, you passed");
+        }
+
+        else {
+            System.out.println("Study harder next time");
+        }
+
+    }
+    
+}
