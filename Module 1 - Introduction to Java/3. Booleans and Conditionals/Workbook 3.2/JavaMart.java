f222619da8c23c84dcfef6b131b0a835cf30574c
Bria Whitehead <whitehead.bria@protonmail.com>
2022-09-13 12:37:41 -0500

    Workbook 3.2 using if else statements with also using prior knowledge of math with Java

--- Module 1 - Introduction to Java/3. Booleans and Conditionals/Workbook 3.2/JavaMart.java
+++ Module 1 - Introduction to Java/3. Booleans and Conditionals/Workbook 3.2/JavaMart.java
@@ -5,20 +5,30 @@ public class JavaMart {
         double toyCar = 5.99;
         System.out.println("Can I get this car?");
        
-        //if you have enough money
-            //       print: Sure!
-            //       pay for the toy car 
+        if (wallet > toyCar) {
+            System.out.println("Sure!");
+            
+            wallet = (wallet - toyCar);
+        }
 
-        //else:  Sorry, I only have <wallet> left.
+        else {
+            System.out.println("Sorry, I have $" + wallet + " left");
+        }
 
         
         double nike = 95.99;       //run test case with 89.99
         System.out.println("Can I get these nike shoes?");
-        //if you have enough money
-            //       print: Sure!
-            //       pay for the nike shoes 
 
-        //else: Sorry, I only have <wallet> left.
+        if (wallet > nike) {
+            System.out.println("Sure!");
+            
+            wallet = (wallet - nike);
+            System.out.println(wallet);
+        }
+
+        else {
+            System.out.println("Sorry, I have $" + wallet + " left");
+        }
 
     }
 }
