23a36d61f6174cf8ecf976136bad1b59da6b4f96
Bria Whitehead <whitehead.bria@protonmail.com>
2022-09-14 14:51:02 -0500

    finished workbook 3.7 on Bank mortgage

--- Module 1 - Introduction to Java/3. Booleans and Conditionals/Workbook 3.7/Bank.java
+++ Module 1 - Introduction to Java/3. Booleans and Conditionals/Workbook 3.7/Bank.java
@@ -3,30 +3,42 @@ import java.util.Scanner;
 public class Bank {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+
         System.out.println("\n****ROYAL BANK OF JAVA****");
+
         System.out.println("Are you here to get a mortgage? (yes or no)");
-        //Task 1 - Pick up the user's decision.
+        String mortgage = scan.nextLine();
+        
+        if(mortgage.equals("yes")) {
 
-
-        //Task 2 - Print this if the decision is "yes"
-        System.out.println("\nGreat! In one line" +
+            System.out.println("\nGreat! In one line" +
             "\nHow much money do you have in your savings?" +
             "\nAnd, how much do you owe in credit card debt?");
-                // Task 3 - Pick up each value 
+
+            double savings = scan.nextDouble();
+            double debt = scan.nextDouble();
+            
+
             System.out.println("\nHow many years have you worked for?");
-               // Task 4 - Pick up number of years
+            int years = scan.nextInt();  // Task 4 - Pick up number of years
+
 
             System.out.println("What is your name?");
-               // Task 5 - Pick up the user's name
-                
-               //Task 6 - Approve the mortgage if they meet the requirements (see article)
-               //       - Otherwise, don't give them a mortgage.          
-               System.out.println("Congratulations <name> You have been approved!");
-               System.out.println("Sorry, you are not eligible for a mortgage.");
+            String name = scan.next();
 
-        //Task 2 - Print this if the decision was not "yes"
+            if(savings >= 10000 && debt < 5000 && years > 1) {
+                System.out.println("Congratulations " + name + "! You have been approved!");
+            }
+
+            else {
+                System.out.println("Sorry, you are not eligible for a mortgage.");
+            }
+            
+            }        
+       
+        else {
         System.out.println("\nOK. Have a nice day!");
-
+        }
 
         scan.close();
     }
