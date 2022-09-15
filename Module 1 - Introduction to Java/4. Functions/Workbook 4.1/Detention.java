4e4bedcc817be90213d40bf2a0a0a7bfa8edbd74
Bria Whitehead <whitehead.bria@protonmail.com>
2022-09-15 14:22:52 -0500

    Finished workbook 4.1. Applying basic function

--- Module 1 - Introduction to Java/4. Functions/Workbook 4.1/Detention.java
+++ Module 1 - Introduction to Java/4. Functions/Workbook 4.1/Detention.java
@@ -1,19 +1,24 @@
 public class Detention {
     public static void main(String[] args) {
-
-        //what Bart did so far
-        System.out.println("I will not copy and paste code.");
-        System.out.println("I will use Camel Case when writing class names.");
-        System.out.println("I will use lower Camel Case when writing function names.");
-        System.out.println("I will use lower Camel Case when writing variables names.\n");
-
-        // Task 2 – call the function 6 times. 
+      
+      printLines();
+      printLines();
+      printLines();
+      printLines();
+      printLines();
+      printLines();
    }
 
 
 
     //Task 1 - Make a function here. See the doc comment for details. 
+    public static void printLines() {
+      System.out.println("I will not copy and paste code.");
+      System.out.println("I will use Camel Case when writing class names.");
+      System.out.println("I will use lower Camel Case when writing function names.");
+      System.out.println("I will use lower Camel Case when writing variables names.\n");
 
+    }
     /**    
       * Funtion name: printLines
       *
