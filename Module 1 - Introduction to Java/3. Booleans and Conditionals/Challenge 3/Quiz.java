c5ffebfc38c8401342f28e497ae83f02485c72cf
Bria Whitehead <whitehead.bria@protonmail.com>
2022-09-15 08:41:28 -0500

    Modified scoring and commented out score counter on questions

--- Module 1 - Introduction to Java/3. Booleans and Conditionals/Challenge 3/Quiz.java
+++ Module 1 - Introduction to Java/3. Booleans and Conditionals/Challenge 3/Quiz.java
@@ -8,19 +8,19 @@ public class Quiz {
 
         System.out.println("1. Which track did Lewis Hamilton come back from essentially a 25 place grid penalty?");
         System.out.println("\ta) COTA\n\tb) Brazil\n\tc) Singapore\n\td) Italy\n");
-        score++;
+        //score++;
 
         System.out.println("\n2. What track did 23XI Racing get their first win as an organization?");
         System.out.println("\ta) Talladega\n\tb) Kansas\n\tc) Daytona\n\td) Miami\n");
-        score++;
+        //score++;
 
         System.out.println("\n3. What year did Lewis Hamilton win his 7th world championship?");
         System.out.println("\ta) 2020\n\tb) 2018\n\tc) 2015\n\td) 2019\n");
-        score++;
+        //score++;
 
         System.out.println("\n4. What number does Lewis Hamilton drive in Formula 1?");
         System.out.println("\ta) 44\n\tb) 23\n\tc) 11\n\td) 45\n");
-        score++;
+        //score++;
 
         System.out.println("\n5. What track did the number 45 sweep in 2022?");
         System.out.println("\ta) Kansas\n\tb) Daytona\n\tc) Miami\n\td) Los Angeles\n");
@@ -36,7 +36,7 @@ public class Quiz {
 
         //Task 2: Check each answer - For each correct answer, add 5 points to the score. (if-else) 
         
-        System.out.println("Your final score is: " + score + "/25");
+        System.out.println("Your final score is: " + score + "/30");
 
         //Task 3: print a message depending on the score
 
