1799f3ac5d1ca36e51028c12f95d6328e292975f
Bria Whitehead <whitehead.bria@protonmail.com>
2022-09-15 14:14:12 -0500

    Completed section 4.1 example

---
+++ Module 1 - Introduction to Java/4. Functions/Section 4/Chorus.java
@@ -0,0 +1,15 @@
+public class Chorus {
+    public static void main(String[] args) {
+        singChorus();
+        singChorus();
+
+    }
+
+
+        public static void singChorus() {
+            System.out.println("list lyrics here");
+            System.out.println("Test test");
+        }
+
+    
+}
