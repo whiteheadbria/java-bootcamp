b02bfda8b2d58bf6497c781ec3dbbb98b9a56321
Bria Whitehead <whitehead.bria@protonmail.com>
2022-09-15 10:54:39 -0500

    Removed correct answers

--- Module 1 - Introduction to Java/3. Booleans and Conditionals/Challenge 3/Quiz.java
+++ Module 1 - Introduction to Java/3. Booleans and Conditionals/Challenge 3/Quiz.java
@@ -7,32 +7,32 @@ public class Quiz {
         int score = 0;
 
         System.out.println("1. Which track did Lewis Hamilton come back from essentially a 25 place grid penalty?");
-        System.out.println("\ta) COTA\n\tb) Brazil\n\tc) Singapore\n\td) Italy\n");
+        System.out.println("\ta) COTA\n\tb) Italy\n\tc) Singapore\n\td) Brazil\n"); 
         //store answer 1
         //score++;
 
         System.out.println("\n2. What track did 23XI Racing get their first win as an organization?");
-        System.out.println("\ta) Talladega\n\tb) Kansas\n\tc) Daytona\n\td) Miami\n");
+        System.out.println("\ta) Talladega\n\tb) Kansas\n\tc) Daytona\n\td) Miami\n"); 
         //store answer 2
         //score++;
 
         System.out.println("\n3. What year did Lewis Hamilton win his 7th world championship?");
-        System.out.println("\ta) 2020\n\tb) 2018\n\tc) 2015\n\td) 2019\n");
+        System.out.println("\ta) 2020\n\tb) 2018\n\tc) 2015\n\td) 2019\n"); 
         //store answer 3
         //score++;
 
         System.out.println("\n4. What number does Lewis Hamilton drive in Formula 1?");
-        System.out.println("\ta) 44\n\tb) 23\n\tc) 11\n\td) 45\n");
+        System.out.println("\ta) 44\n\tb) 23\n\tc) 11\n\td) 45\n"); 
         //store answer 4
         //score++;
 
         System.out.println("\n5. What track did the number 45 sweep in 2022?");
-        System.out.println("\ta) Kansas\n\tb) Daytona\n\tc) Miami\n\td) Los Angeles\n");
+        System.out.println("\ta) Kansas\n\tb) Daytona\n\tc) Miami\n\td) Los Angeles\n"); 
         //store answer 5
         //score++
 
         System.out.println("\n6. Which drivers are tied for seven World Championships in Formula 1?");
-        System.out.println("\ta) Lewis Hamilton\n\tb) George Russell\n\tc) Michael Schumacher\n\td) Sebastian Vettel\n");
+        System.out.println("\ta) Lewis Hamilton\n\tb) George Russell\n\tc) Michael Schumacher\n\td) Sebastian Vettel\n"); 
         //store answer 6
         //score++
         
