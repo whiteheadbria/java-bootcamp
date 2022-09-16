d237340bd42a76c4320a6d760cbae9aa5664c58f
Bria Whitehead <whitehead.bria@protonmail.com>
2022-09-16 11:12:55 -0500

    Learning about built in functions, just practice

---
+++ Module 1 - Introduction to Java/4. Functions/Section 4/BuiltInFunctions.java
@@ -0,0 +1,21 @@
+public class BuiltInFunctions {
+    public static void main(String[] args) {
+        System.out.println("Hello Java");
+
+        double sine = Math.sin(1.2);
+        System.out.println("The sine of 1.2 is: " + sine);
+
+        double power = Math.pow(2,4);
+        System.out.println("2 to the power of 4 is: " + power);
+
+        double random = Math.random();//prints random number between 0 - 1
+        System.out.println("The random value is: " + random);
+
+        double random1 = Math.random() * 100 ;//prints random number between 0 - 100
+        System.out.println("The random value is: " + random1);
+
+        
+
+    }
+    
+}
