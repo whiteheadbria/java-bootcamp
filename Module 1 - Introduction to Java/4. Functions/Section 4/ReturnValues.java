44f805e795b929af3ea7cd6f53f8fbdde1db9c1a
Bria Whitehead <whitehead.bria@protonmail.com>
2022-09-16 09:23:42 -0500

    Worked with switch cases and if-else in returns

--- Module 1 - Introduction to Java/4. Functions/Section 4/ReturnValues.java
+++ Module 1 - Introduction to Java/4. Functions/Section 4/ReturnValues.java
@@ -1,26 +1,32 @@
 public class ReturnValues {
     public static void main(String[] args) {
-        double measure1 = measureArea(4.5, 8.3);
-        double measure2 = measureArea(8.5, 8.3);
-        double measure3 = measureArea(9.3, 8.3);
-        double measure4 = measureArea(7.8, 8.3);
-        double measure5 = measureArea(8.3, 8.3);
+        double measure1 = measureArea(4.3, 2.2, "area");
+        double measure2 = measureArea(3.2, 4.1, "perimeter");
 
-        stringPrinter(4.5, 8.3, measure1);
-        stringPrinter(8.5, 8.3, measure2);
-        stringPrinter(9.3, 8.3, measure3);
-        stringPrinter(7.8, 8.3, measure4);
-        stringPrinter(8.3, 8.3, measure5);
+
+        stringPrinter(4.3, 2.2, measure1, "area");
+        stringPrinter(3.2, 4.1, measure2, "perimeter");
+
 
     }
 
-    public static double measureArea(double length, double width) {
-        double area = length * width;
-        return area;
+    public static double measureArea(double length, double width, String option) {
+        if (length < 0 || width < 0) {
+            System.out.println("length or width cannot be negative");
+            System.exit(0);
+        }
+
+        switch(option) {
+            case "area":return length * width;
+            case "perimeter":return 2 * (length + width);
+            default: return 404;
+        }
     }
 
-    public static void stringPrinter(double length, double width, double area) {
-        System.out.println("When the length of a rectangle is: " + length + " and the width is: " + width + " the area is: " + area);
+    public static void stringPrinter(double length, double width, double measure, String option) {
+        System.out.println("When the length is " + length + " and the width is: " + width + " the " + option + " is: " + measure);
 
     }
+
+    //stringPrinter recieves the String option aruguement as a parameter and it inserts it into a longer string 
 }
