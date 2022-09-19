0b6d2db9d0794c951d09a628e9e7a7b0d0a18552
whiteheadbria <whitehead.bria@protonmail.com>
2022-09-19 10:03:08 -0500

    in progress not done. pushing from Mac to GitHub

--- Module 1 - Introduction to Java/4. Functions/Challenge 4/RockPaperScissors.java
+++ Module 1 - Introduction to Java/4. Functions/Challenge 4/RockPaperScissors.java
@@ -10,22 +10,29 @@ public class RockPaperScissors {
         System.out.println("Are you ready? Write 'yes' if you are.");
 
     //Task 1: See if the user wants to play. 
+        String play = scan.nextLine();
 
     /*Task 2: Set up the game
-    
-
-       • if the answer is yes: 
-             – print: Great!
-             – print: rock – paper – scissors, shoot!
-             – pick up user's choice.
              – get the computer choice (can only be done after task 3).
              – get the result (can only be done after task 4)
-             – print everything (can only be done after task 5).
-
-       • else:
-             – print: Darn, some other time...!        
+             – print everything (can only be done after task 5).   
     */
+
+    if(play.equals("yes") || play.equals("YES") || play.equals("Yes")) 
+    {
+        System.out.println("\nGreat!");
+        System.out.println("rock - paper - scissors, shoot!"); 
+        String choice = scan.nextLine();
+        String computerChoice = computerChoice();
+
+        System.out.println("\n You choose:\t" + choice);
+        System.out.println("\n The computer chose:\t" + computerChoice);
+    }
               
+    else {
+        System.out.println("Darn, some other time I  guess!");
+        System.exit(0);
+    }
 
         scan.close();
     }
@@ -33,19 +40,18 @@ public class RockPaperScissors {
 
     //Task 3  – Write a function where the computer picks a random choice.
 
-    /**
-     * Function name: computerChoice - picks randomly between rock paper and scissors
-     * @return a choice (String).
-     * 
-     * Inside the function:
-     *   1. Picks a random number between 0 and 2.
-     *   2. if 0: returns the choice 'rock'
-     *      if 1: returns the choice 'paper'
-     *      if 2: returns the choice 'scissors'
-     */
+     public static String computerChoice() {
+        double randomNumber = Math.random() * 3; // range: 0 - 2.9999
+        int integer = (int)randomNumber; // range: 0 -2
 
-
-
+        switch (integer) {
+            case 0: return "rock";
+            case 1: return "paper";
+            case 2: return "scissors";
+            default: return ""; // not possible
+        }
+        
+     }
 
     //Task 4  – Write a function that compares the choices and returns the result.
 
