3295ba2b82fd87dbdee54b7d9e5ba9822b098246
whiteheadbria <whitehead.bria@protonmail.com>
2022-09-23 15:43:30 -0500

    Completed Workbook 5.6

--- Module 1 - Introduction to Java/5. Loops/Workbook 5.6/EvenOrOdd.java
+++ Module 1 - Introduction to Java/5. Loops/Workbook 5.6/EvenOrOdd.java
@@ -1,13 +1,22 @@
 public class EvenOrOdd {
     public static void main(String[] args) {
-        // Task 1 – Make a for loop that counts from 0 to 19. 
-        
-        /* Task 2
-             
-            1. If the number is even, print ' – even' beside the number.
-            2. If the number is odd, print ' – odd' beside the number.
+        // Task 1 – Make a for loop that counts from 0 to 19.
+        for(int i = 0; i < 20; i++) {
+            System.out.println(i);
+        }
 
-         */
+        System.out.println("\n");
+
+        for(int j = 0; j < 20; j++) {
+
+        if(j % 2 == 0) {
+            System.out.println(j + ": even");
+        }
+
+        else {
+            System.out.println(j + ": odd");
+        }  
 
     }
+}
 }
