3fac9bf0686fb9e1ce35db48f5fee57d194203c1
whiteheadbria <whitehead.bria@protonmail.com>
2022-09-23 14:42:59 -0500

    Beer song loop. Workbook 5.4 completed

--- Module 1 - Introduction to Java/5. Loops/Workbook 5.4/Beer.java
+++ Module 1 - Introduction to Java/5. Loops/Workbook 5.4/Beer.java
@@ -1,17 +1,12 @@
 public class Beer {
     public static void main(String[] args) {
        // Task 2: make a for loop that starts at i = 99, and finishes at i=1.
+       for(int i = 99; i >= 1; i--) {
+         sing(i);
+       }
     }
 
-    /** Task 1
-     * Function name – sing.
-     * @param i (int)
-     * 
-     * 
-     * Inside the function: 
-     *  1. print:
-     *      <i> bottles of beer on the wall, <i> bottles of beer!
-     *      take one down, pass it around, <i-1> bottles of beer on the wall!
-     */
-
+    public static void sing(int i) {
+         System.out.println(i  + " bottles of been on the wall, " + i + " bottles of beer!\n take on down, pass it around, " + (i -1) + " beer on the wall");
+    }
 }
