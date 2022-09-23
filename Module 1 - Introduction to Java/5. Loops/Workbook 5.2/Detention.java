04728f11541626fb15cc433288faa36d483c5062
whiteheadbria <whitehead.bria@protonmail.com>
2022-09-23 12:53:01 -0500

    Completed Workbook 5.2

--- Module 1 - Introduction to Java/5. Loops/Workbook 5.2/Detention.java
+++ Module 1 - Introduction to Java/5. Loops/Workbook 5.2/Detention.java
@@ -1,8 +1,22 @@
+import java.util.Scanner;
+
 public class Detention {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
 
         System.out.println("Hi Bart. I can write lines for you.\nWhat do you want me to write?");
+        String lines = scan.nextLine();
         //Task 1 – Scan for the nextLine(), and print it 99 times. 
         
+
+        for(int i = 0; i <= 99; i++) {
+            System.out.println(i + ". " + lines);
+        }
+
+        scan.close();
+        
+        
+        }
+        
     }
-}
+
