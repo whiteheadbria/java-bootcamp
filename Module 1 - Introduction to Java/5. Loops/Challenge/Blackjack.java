b763431dd9bd791ec6310f1185fb7233322e17ae
Bria Whitehead <whitehead.bria@protonmail.com>
2022-09-27 16:56:31 -0500

    Still working on the Black Jack Challenge

--- Module 1 - Introduction to Java/5. Loops/Challenge/Blackjack.java
+++ Module 1 - Introduction to Java/5. Loops/Challenge/Blackjack.java
@@ -10,16 +10,40 @@ public class Blackjack {
         System.out.println("We shall see..");
         System.out.println("..Ready? Press anything to begin!");
         //Task 3 – Wait for the user to press enter.
+        scan.nextLine();
+
         //Task 4 – Get two random cards.
         //       – Print them: \n You get a \n" + <randomCard> + "\n and a \n" + <randomCard>
+        int card1 = drawRandomCard();
+        int card2 = drawRandomCard();
+
+        System.out.println("You get a \n" + cardString(card1) + "\n and a \n" + cardString(card2));
+
 
         //Task 5 – Print the sum of your hand value.
         //       – print: your total is: <hand value>
+        // face cards (Jack, Queen, King) high values of 10
+        int total = Math.min(card1, 10) + Math.min(card2, 10);
+        System.out.println("Your total is: " + total + "\n");
+
 
         
         //Task 6 – Get two random cards for the dealer.
         //       – Print: The dealer shows \n" + <first card> + "\nand has a card facing down \n" + <facedown card>
         //       – Print: \nThe dealer's total is hidden
+        int dealerCard1 = drawRandomCard();
+        int dealerCard2 = drawRandomCard();
+
+        System.out.println("You get a \n" + cardString(dealerCard1) + "\n and a \n" + cardString(dealerCard2));
+        int dealerTotal = Math.min(card1, 10) + Math.min(card2, 10);
+        System.out.println("The dealers's total is hidden\n");
+       
+
+        String option = hitOrStay();
+
+       
+
+    
 
        
         //Task 8 – Keep asking the player to hit or stay (while loop).
@@ -46,6 +70,13 @@ public class Blackjack {
      *   2. Returns a card.
      */
 
+     public static int drawRandomCard() {
+        double randomNumber = Math.random() * 13;
+        randomNumber += 1;
+
+        return (int)randomNumber;
+     }
+
     /** Task 2 – make a function that returns a String drawing of the card.
      * Function name – cardString
      * @param cardNumber (int)
@@ -54,6 +85,133 @@ public class Blackjack {
      * Inside the function:
      *   1. Returns a String drawing of the card.
      */
+
+     public static String cardString(int cardNumber) {
+        switch(cardNumber) {
+        case 1: 
+        return 
+        "   _____\n"+
+        "  |A _  |\n"+ 
+        "  | ( ) |\n"+
+        "  |(_'_)|\n"+
+        "  |  |  |\n"+
+        "  |____V|\n";
+        
+
+        case 2:
+        return   
+        "   _____\n"+              
+        "  |2    |\n"+ 
+        "  |  o  |\n"+
+        "  |     |\n"+
+        "  |  o  |\n"+
+        "  |____Z|\n";
+
+        case 3:
+        return 
+        "   _____\n" +
+        "  |3    |\n"+
+        "  | o o |\n"+
+        "  |     |\n"+
+        "  |  o  |\n"+
+        "  |____E|\n";
+
+        case 4:
+        return  
+        "   _____\n" +
+        "  |4    |\n"+
+        "  | o o |\n"+
+        "  |     |\n"+
+        "  | o o |\n"+
+        "  |____h|\n";
+
+        case 5:
+        return   
+        "   _____ \n" +
+        "  |5    |\n" +
+        "  | o o |\n" +
+        "  |  o  |\n" +
+        "  | o o |\n" +
+        "  |____S|\n";
+
+        case 6:
+        return      
+        "   _____ \n" +
+        "  |6    |\n" +
+        "  | o o |\n" +
+        "  | o o |\n" +
+        "  | o o |\n" +
+        "  |____6|\n";
+
+        case 7:
+        return  
+        "   _____ \n" +
+        "  |7    |\n" +
+        "  | o o |\n" +
+        "  |o o o|\n" +
+        "  | o o |\n" +
+        "  |____7|\n";
+
+        case 8: 
+        return   
+        "   _____ \n" +
+        "  |8    |\n" +
+        "  |o o o|\n" +
+        "  | o o |\n" +
+        "  |o o o|\n" +
+        "  |____8|\n";
+
+        case 9:
+        return   
+        "   _____ \n" +
+        "  |9    |\n" +
+        "  |o o o|\n" +
+        "  |o o o|\n" +
+        "  |o o o|\n" +
+        "  |____9|\n";
+
+        case 10:
+        return     
+        "   _____ \n" +
+        "  |10  o|\n" +
+        "  |o o o|\n" +
+        "  |o o o|\n" +
+        "  |o o o|\n" +
+        "  |___10|\n";
+
+        case 11:
+        return     
+        "   _____\n" +
+        "  |J  ww|\n"+ 
+        "  | o {)|\n"+ 
+        "  |o o% |\n"+ 
+        "  | | % |\n"+ 
+        "  |__%%[|\n";
+
+        case 12:
+        return  
+        "   _____\n" +
+        "  |Q  ww|\n"+ 
+        "  | o {(|\n"+ 
+        "  |o o%%|\n"+ 
+        "  | |%%%|\n"+ 
+        "  |_%%%O|\n";
+
+        case 13: 
+        return 
+        "   _____\n" +
+        "  |K  WW|\n"+ 
+        "  | o {)|\n"+ 
+        "  |o o%%|\n"+ 
+        "  | |%%%|\n"+ 
+        "  |_%%%>|\n";
+
+        default:
+        return "This shouldnt be called";        
+        }
+        
+    }
+    
 
     public static String faceDown() {
         return
@@ -75,5 +233,18 @@ public class Blackjack {
      *      Please write 'hit' or 'stay'
      *   3. Returns the user's option 
      */
+
+     public static String hitOrStay() {
+
+        System.out.println("Would you like to hit or stay?");
+        String response = scan.nextLine();
+
+        while(!(response.equalsIgnoreCase("hit") || response.equalsIgnoreCase("stay"))) {
+            System.out.println("Please type hit or stay");
+            response = scan.nextLine();
+        }
+
+        return response;
+    } 
     }
 
