d3c9ed84a2191d253600570d9c79301562bf3d13
Bria Whitehead <whitehead.bria@protonmail.com>
2022-09-27 11:48:05 -0500

    Workbook 5.11 complete

--- Module 1 - Introduction to Java/5. Loops/Workbook 5.11/JavaRolls.java
+++ Module 1 - Introduction to Java/5. Loops/Workbook 5.11/JavaRolls.java
@@ -3,11 +3,10 @@ import java.util.Scanner;
 public class JavaRolls {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-      
-        /* Task 1: 
-              1. print: Let's play Rolling Java. Type anything to start.
-              2. Get the user to enter any value
-        */
+
+
+        System.out.println("Let's play Rolling Java. Press anything to begin");
+        scan.nextLine();
 
         /* Task 2: 
               1. Once the user is ready, print: 
@@ -19,54 +18,59 @@ public class JavaRolls {
               2. Get the user to enter any value
         */
 
+        System.out.println("Great, here are the rules:\n");
+        System.out.println("- If you roll a 6 the game stops");
+        System.out.println("- If you roll a 4 you get 0 points");
+        System.out.println("- Otherwise, you get 1 point\n");
+        System.out.println("You must collect at least 3 points to win. Press anything to roll the dice");        
+
        int score = 0;
-      
-       /* 
-         Task 3
-          1. Make a while loop that runs forever
-          2. During each run, get them to enter a random value
-          
-          
-          Task 5
-           1. During each run, call the rollDice() function.
-           2. Print each dice roll like so: You rolled a <diceRoll>.           
-           
-          
-          Task 6
-            If the user rolls a 6:
-              1. print: End of game.
-              2. stop the game. 
 
-            If the user rolls a 4:
-              1. print: Zero points. Keep rolling. 
+       while(true) {
+        scan.nextLine();
+        int diceRoll = rollDice();
+        System.out.println("You rolled a " + diceRoll + ". ");
 
-            If the user rolls anything else:
-              1. update the points variable by 1
-              2. print: One point. Keep rolling. 
+        if (diceRoll == 6) {
+          System.out.println("End of game");
+          break;
+        }
 
-       */
+          else if (diceRoll == 4) {
+            System.out.println("Zero points. Keep rolling");
+            continue;
+          }
 
-      
-      /* Task 7
-      
-           1.  After the game ends, check the user's points.
-       
-                 System.out.println("\nYour score is: " + score);
+          else {
+            score++;
+            System.out.println("One point. Keep rolling");
+            continue;
+          }
+        }
 
-           2. If the score is greater than or equal to 3, print:
-                  Wow, that's lucky. You win!
-              Otherwise, print:
-                  Tough luck, you lose :( 
-      */
+        System.out.println("\nYour score is: " + score);
 
-    }
-  
-  
+        if(score >= 3) {
+          System.out.println("Wow, you won!");
+        }
+
+        else {
+          System.out.println("You lose. Hopefully things roll around better next time.");
+        }
+        
+       }
+ 
     /** Task 4
      * Function name: rollDice – rolls a dice between 1 and 6
      * @return randomNumber (int)
      * 
      */ 
+
+     public static int rollDice() {
+      double randomNumber = Math.random() * 6;
+      randomNumber = randomNumber + 1;
+
+      return (int)randomNumber;
+     }
   
-  
-}
+    }
