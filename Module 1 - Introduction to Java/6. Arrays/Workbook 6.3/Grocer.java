2823a6e8c329a058aafc39de2957f55b6f459a5c
whiteheadbria <whitehead.bria@protonmail.com>
2022-09-30 16:23:35 -0500

    Workboko 6.3 complete

--- Module 1 - Introduction to Java/6. Arrays/Workbook 6.3/Grocer.java
+++ Module 1 - Introduction to Java/6. Arrays/Workbook 6.3/Grocer.java
@@ -6,8 +6,14 @@ public class Grocer {
         /* Task 1: Create an array that stores each aisle:
              apples – bananas – candy – chocolate – coffee – tea
 
+
              Then, use a for loop to print each element in the array. 
         */
+
+        String[] snacks = {"apples", "bananas", "candy", "chocolate", "coffee", "tea"};
+        for(int i = 0; i < snacks.length; i++) {
+            System.out.println(snacks[i]);
+        }
  
         /* Task 2:
            1. System.out.println("\nDo you sell coffee?");
@@ -17,6 +23,17 @@ public class Grocer {
             • if so, break the loop and print: \nWe have that in aisle: <index>
 
         */
+
+        System.out.print("\nDo you sell coffee?");
+        
+
+        for(int i = 0; i< snacks.length; i++) {
+            if(snacks[i].equals("coffee")) {
+                System.out.println("We have coffee at aisle: " + i); 
+                break;
+            }
+
+        }
     
     }
 }
