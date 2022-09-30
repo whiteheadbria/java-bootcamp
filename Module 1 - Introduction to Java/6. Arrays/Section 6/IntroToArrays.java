ccf7b800b8ab61376932721d54ab22a66b8d2b91
Bria Whitehead <whitehead.bria@protonmail.com>
2022-09-30 10:03:08 -0500

    Section 6 Intro

---
+++ Module 1 - Introduction to Java/6. Arrays/Section 6/IntroToArrays.java
@@ -0,0 +1,7 @@
+class IntroToArrays {
+    public static void main(String[] args) {
+        String[] formulaOneTeams = {"Mercedes", "Aston Martin", "Haas", "Williams"};
+        System.out.println(formulaOneTeams[0]);
+
+    }
+}
