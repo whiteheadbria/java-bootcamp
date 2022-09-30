f297b134e9a801ab5ab331969639fedd2d5767fa
whiteheadbria <whitehead.bria@protonmail.com>
2022-09-30 14:09:35 -0500

    Created ArrayAndLoops file to work with arrays and loops

---
+++ Module 1 - Introduction to Java/6. Arrays/Section 6/ArraysAndLoops.java
@@ -0,0 +1,10 @@
+class ArraysAndLoops {
+    public static void main(String[] args) {
+        String[] drivers = {"Lewis", "George", "Charles", "Carlos"};
+        System.out.println("The number of elements is: " + drivers.length);
+        for(int i = 0; i < drivers.length; i++) {
+            System.out.println("The element at index " + i + " is Formula One Driver " + drivers[i]);
+        }
+
+    }
+}
