7f774fab4bbdfdc47c9d44458d61626ce6f80cde
whiteheadbria <whitehead.bria@protonmail.com>
2022-09-30 16:08:44 -0500

    Workbook 6.2 complete

--- Module 1 - Introduction to Java/6. Arrays/Workbook 6.2/Exams.java
+++ Module 1 - Introduction to Java/6. Arrays/Workbook 6.2/Exams.java
@@ -3,6 +3,13 @@ public class Exams {
         
         System.out.println("It's time to take your 5th years exams. Please, take your seats.\n");
         //Task 1: Create an array that stores 5 students: "Malfoy", "Crabbe", "Goyle", "Pansy", "Dean"
+        String[] drivers = {"Lewis", "George", "Charles", "Carlos", "Zhou"};
+        System.out.println(drivers.length);
+        
+
+        for(int i = 0; i < drivers.length; i++) {
+            System.out.println(drivers[i] + ", you will take seat " + i);
+        }
         
         /**Task 2: Using a for loop, assign each student a seat according to their index in the array.
          
