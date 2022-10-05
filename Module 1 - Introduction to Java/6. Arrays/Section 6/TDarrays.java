7c9823ea97d10ee883a48a262bf97d534b3234a8
Bria Whitehead <whitehead.bria@protonmail.com>
2022-10-05 10:25:11 -0500

    Workbook for 2D arrays

---
+++ Module 1 - Introduction to Java/6. Arrays/Section 6/TDarrays.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class TDarrays {
+    public static void main(String[] args) {
+        int[][] grades = {
+            {44, 45, 23, 11},
+            {65, 64, 63, 62}, 
+            {78, 79, 80, 81} 
+        };
+
+        System.out.println("\tLewis: " + grades[0][0] + " " + grades[0][1] + " " + grades[0][2] + " " + grades[0][3]);
+        System.out.println("\tGeorge: " + grades[1][0] + " " + grades[1][1] + " " + grades[1][2] + " " + grades[1][3]);
+        System.out.println("\tToto: " + grades[2][0] + " " + grades[2][1] + " " + grades[2][2] + " " + grades[2][3]);
+
+    }
+    
+}
