f25faa6540df0df318e6d6c18a6994e29e1a4ce0
Bria Whitehead <whitehead.bria@protonmail.com>
2022-10-05 10:47:18 -0500

    Redo 2D Arrays to be more effective and readable

--- Module 1 - Introduction to Java/6. Arrays/Section 6/TDarrays.java
+++ Module 1 - Introduction to Java/6. Arrays/Section 6/TDarrays.java
@@ -8,10 +8,18 @@ public class TDarrays {
             {78, 79, 80, 81} 
         };
 
-        System.out.println("\tLewis: " + grades[0][0] + " " + grades[0][1] + " " + grades[0][2] + " " + grades[0][3]);
-        System.out.println("\tGeorge: " + grades[1][0] + " " + grades[1][1] + " " + grades[1][2] + " " + grades[1][3]);
-        System.out.println("\tToto: " + grades[2][0] + " " + grades[2][1] + " " + grades[2][2] + " " + grades[2][3]);
+        for(int i = 0; i < grades.length; i++) {
 
+            switch (i) {
+                case 0: System.out.print("\tLewis: "); break;
+                case 1: System.out.print("\tGeorge: "); break;
+                case 2: System.out.print("\tCharles: "); break;
+            }
+            for(int j = 0; j < grades[i].length; j++) {
+                System.out.print(grades[i][j] + " ");
+            }
+            System.out.print("\n");
     }
-    
+}
+
 }
