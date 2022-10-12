83533857e612501beab9d6da4a9c328519fb4219
whiteheadbria <whitehead.bria@protonmail.com>
2022-10-11 19:56:38 -0500

    Working with objects

--- Module 2 - Object Oriented Programming/7. The World of Objects/Section Seven/Car.java
+++ Module 2 - Object Oriented Programming/7. The World of Objects/Section Seven/Car.java
@@ -3,9 +3,10 @@ public class Car {
     double price;
     int year;
     String color;
-    public static void main(String[] args) {
-        
+    
+    public Car (String make, double price, int year, String color) {
+
+
         
     }
-    
 }
--- Module 2 - Object Oriented Programming/7. The World of Objects/Section Seven/Main.java
+++ Module 2 - Object Oriented Programming/7. The World of Objects/Section Seven/Main.java
@@ -1,17 +1,7 @@
 public class Main {
     public static void main (String[] args) {
-        Car jeep = new Car(); //Java creates an object of the car class
-
-        jeep.make = "Jeep";
-        jeep.price = 30000;
-        jeep.year = 2022;
-        jeep.color = "Slate Grey";
-
-        Car bronco = new Car();
-        bronco.make = "Bronco";
-        bronco.price = 32000;
-        bronco.year = 2022;
-        bronco.color = "Area 51";
+        Car jeep = new Car("Jeep", 30000, 2022, "Slate Grey"); //Java creates an object of the car class
+        Car bronco = new Car("Bronco", 32000, 2022, "Area 51");
 
         System.out.println("This " + jeep.make + " is worth $" + jeep.price + ". It was manufactured in " + jeep.year + " and the color of the Jeep is " + jeep.color);
         System.out.println("This " + bronco.make + " is worth $" + bronco.price + ". It was manufactured in " + bronco.year + " and the color of the Bronco is " + bronco.color);
