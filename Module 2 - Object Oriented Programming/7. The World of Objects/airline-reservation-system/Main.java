772ae42f56b9f4776144124209043061ce2fd23f
whiteheadbria <whitehead.bria@protonmail.com>
2022-10-11 19:57:10 -0500

    Working with objects

--- Module 2 - Object Oriented Programming/7. The World of Objects/airline-reservation-system/Main.java
+++ Module 2 - Object Oriented Programming/7. The World of Objects/airline-reservation-system/Main.java
@@ -1,6 +1,22 @@
+import java.util.Arrays;
+
 public class Main {
-  
+
     public static void main(String[] args) {
+
+        Person briona = new Person();
+        briona.name = "Briona";
+        briona.nationality = "American";
+        briona.dateOfBirth = "01/21/1998";
+        briona.passport = new String[] {briona.name, briona.nationality, briona.dateOfBirth};
+        briona.seatNumber  = 44;
+
+        System.out.println(briona.name);
+        System.out.println(briona.nationality);
+        System.out.println(briona.dateOfBirth);
+        System.out.println(Arrays.toString(briona.passport));
+        System.out.println(briona.seatNumber);
+
 
 
       
---
+++ Module 2 - Object Oriented Programming/7. The World of Objects/airline-reservation-system/Person.java
@@ -0,0 +1,13 @@
+public class Person {
+    String name;
+    String nationality;
+    String dateOfBirth;
+    String[] passport;
+    int seatNumber;
+    //applyPassport();
+    //chooseSeat();
+
+    public static void main(String[] args) {
+    }
+    
+}
