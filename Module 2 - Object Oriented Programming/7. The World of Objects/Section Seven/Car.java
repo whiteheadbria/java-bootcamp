e43d82f553464841cc664b459f3426af49ea5e89
Bria Whitehead <whitehead.bria@protonmail.com>
2022-10-21 11:32:17 -0500

    Practicing this parameter

--- Module 2 - Object Oriented Programming/7. The World of Objects/Section Seven/Car.java
+++ Module 2 - Object Oriented Programming/7. The World of Objects/Section Seven/Car.java
@@ -1,10 +1,15 @@
 public class Car {
+
     String make;
     double price;
     int year;
     String color;
     
-    public Car (String make, double price, int year, String color) {
+    public Car(String make, double price, int year, String color) {
+        this.make = make;
+        this.price = price;
+        this.year = year;
+        this.color = color;
 
 
         
