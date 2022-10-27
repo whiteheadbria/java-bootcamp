71ddd18977b9c8dccf914269b32e01edac4de6f2
Bria Whitehead <whitehead.bria@protonmail.com>
2022-10-27 11:24:06 -0500

    Pushing commits from workbooks and sections

--- Module 2 - Object Oriented Programming/7. The World of Objects/Section Seven/Car.java
+++ Module 2 - Object Oriented Programming/7. The World of Objects/Section Seven/Car.java
@@ -1,17 +1,78 @@
+import java.util.Arrays;
+
 public class Car {
 
-    String make;
-    double price;
-    int year;
-    String color;
+    private String make;
+    private double price;
+    private int year;
+    private String color;
+    private String[] parts; 
     
-    public Car(String make, double price, int year, String color) {
+    public Car(String make, double price, int year, String color, String[] parts) {
         this.make = make;
         this.price = price;
         this.year = year;
         this.color = color;
-
-
+        this.parts = Arrays.copyOf(parts, parts.length);
         
     }
+
+    public Car(Car source) { //Copy constructor
+        this.make = source.make;
+        this.price = source.price;
+        this.year = source.year;
+        this.color = color;
+        this.parts = Arrays.copyOf(source.parts, source.parts.length);
+
+    }
+
+    public String getMake() {
+        return this.make;
+    }
+
+    public double getPrice() {
+        return this.price;
+    }
+
+    public int getYear() {
+        return this.year;
+    }
+
+    public String getColor() {
+        return this.color;
+    }
+
+    public String[] getParts() {
+        return Arrays.copyOf(this.parts, this.parts.length);
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setParts(String[] parts) {
+        this.parts = Arrays.copyOf(parts, parts.length);
+    }
+    public void drive() {
+        System.out.println("You brought the beautiful " + this.year + " " + this.color
+        + " " + this.make + " for " + this.price + ".");
+        System.out.println("Drive to the nearest exit\n");
+    }
+
+
+
+
+
 }
--- Module 2 - Object Oriented Programming/7. The World of Objects/Section Seven/Main.java
+++ Module 2 - Object Oriented Programming/7. The World of Objects/Section Seven/Main.java
@@ -1,10 +1,18 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main (String[] args) {
-        Car jeep = new Car("Jeep", 30000, 2022, "Slate Grey"); //Java creates an object of the car class
-        Car bronco = new Car("Bronco", 32000, 2022, "Area 51");
 
-        System.out.println("This " + jeep.make + " is worth $" + jeep.price + ". It was manufactured in " + jeep.year + " and the color of the Jeep is " + jeep.color);
-        System.out.println("This " + bronco.make + " is worth $" + bronco.price + ". It was manufactured in " + bronco.year + " and the color of the Bronco is " + bronco.color);
+        String[] parts ={"tires", "keys"};
 
-    }
+        Car jeep = new Car("Jeep", 30000, 2022, "Slate Grey", parts); //Java creates an object of the car class
+        Car bronco = new Car("Bronco", 32000, 2022, "Area 51", parts);
+        Car jeep2 = new Car(jeep);
+        jeep2.setColor("Jet Black");
+        jeep2.setParts(new String[] {"tires", "filter"}); // return new String[] {"1", "2", ...}
+
+
+        System.out.println(Arrays.toString(jeep2.getParts()));
+
+     }
 }
