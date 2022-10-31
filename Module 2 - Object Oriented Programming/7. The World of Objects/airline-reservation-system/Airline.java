3de6f0e6355d4f2a8ab9d6b2b7b7a2f608553d1a
Bria Whitehead <whitehead.bria@protonmail.com>
2022-10-31 08:35:08 -0500

    Workbook not finihsed

--- Module 2 - Object Oriented Programming/7. The World of Objects/airline-reservation-system/Airline.java
+++ Module 2 - Object Oriented Programming/7. The World of Objects/airline-reservation-system/Airline.java
@@ -1,6 +1,11 @@
 public class Airline {
-    Person[] seats;
+    private Person[] seats; //Airline object is described by the people it arranges seats for
    // createReservation();
+
+   public Airline() {
+    
+   }
     
     
 }
+//workbook not complete
